35c8c6119a520f4da59db8436461fc77ffe734aa
Mary Ann Natividad <maryannbernardo212@gmail.com>
2021-10-26 22:58:50 +0800

    <message>

---
+++ MyCalculator/MyCalculator/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyCalculator
+{
+    public partial class MyCalculator : Form
+    {
+        public MyCalculator()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+        
+        }
+
+        private void button30_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
